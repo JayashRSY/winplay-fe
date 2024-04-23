b19a77fb2f25880d64a1197a009b79c703501ff7
Jayash Yadav <jayash.yadav@geminisolutions.com>
2024-04-23 18:45:54 +0530

    added apiURL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://displate-be.vercel.app/api',
+    apiUrl: window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://winplay-be.onrender.com/api',
     googleClientID: "709452327599-ks9gb4i9hf9f9nungnfhl2u2pqc67iic.apps.googleusercontent.com",
 }
